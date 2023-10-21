9ddc2a8b078264da74a4acd1a3958f2238d86d43
Gabriel Ruas <gabrielamaralruas@gmail.com>
2023-10-21 13:37:51 +0100

    + publisher_example

--- my_robot_controller/my_robot_controller/my_first_node.py
+++ my_robot_controller/my_robot_controller/my_first_node.py
@@ -1,4 +1,4 @@
-#! usr/bin/env python3
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 
@@ -7,6 +7,12 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("first_node");
         self.get_logger().info("Hello from ROS2")
+        self.create_timer(1.0, self.timer_callback) 
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
+
+    
 
 def main(args=None):
     rclpy.init(args=args)
